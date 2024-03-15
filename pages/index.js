d892d1669a78e633a4947d8e4dcc7d709147c753
Lucas Henrique <130518705+lucashenrique505@users.noreply.github.com>
2024-03-15 01:20:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 function Home() {
-    return <h1>
-        Amanda, eu amo você. Se você me ama, dá uma risadinha! 😎
-    </h1>
+  return <h1>Amanda, eu amo você. Se você me ama, dá uma risadinha! 😎</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
